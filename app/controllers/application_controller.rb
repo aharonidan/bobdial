0524c8cb833bc9f4bc52c28fa21a84f4552a321a
Dan Aharoni <aharonidan@gmail.com>
2018-06-19 16:51:11 +0100

    log user details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :print_user_details
   include SessionsHelper
 
   def redirect_to_login
     redirect_to login_path
+  end
+
+  def print_user_details
+  	if current_user
+  		puts '-'*80
+	  	puts current_user.name
+	  	puts self.class.to_s + "." + self.action_name
+	  	puts '-'*80
+	  end
   end
 end
