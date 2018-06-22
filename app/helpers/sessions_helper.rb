f196c2802f56c0ad697fbf8ec48c70d0b7d2f12c
Dan Aharoni <aharonidan@gmail.com>
2018-06-22 12:21:20 +0100

    remove peru from grey horse

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -48,6 +48,7 @@ module SessionsHelper
     return false if title == :black_horse and user.black_horse and user.black_horse.name == 'morocco'
     return false if title == :black_horse and user.black_horse and user.black_horse.name == 'saudi_arabia'
     return false if title == :grey_horse and user.grey_horse and user.grey_horse.name == 'egypt'
+    return false if title == :grey_horse and user.grey_horse and user.grey_horse.name == 'peru'
 
     return true if not Game.send("#{title}_announced?")
     user.send("#{title}_points") > 0
