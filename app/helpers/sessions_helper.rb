6d5341efe7702ea387b3415c1024dfa020508c30
Dan Aharoni <aharonidan@gmail.com>
2018-07-02 18:16:09 +0100

    after the army trip update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -63,6 +63,7 @@ module SessionsHelper
     return false if title == :top_scorer and user.top_scorer and user.top_scorer == 'messi'
     return false if title == :top_scorer and user.top_scorer and user.top_scorer == 'ronaldo'
     return false if title == :after_army_trip and user.after_army_trip and user.after_army_trip == 'africa'
+    return false if title == :after_army_trip and user.after_army_trip and user.after_army_trip == 'central_america'
 
     return true if not Game.send("#{title}_announced?")
     user.send("#{title}_points") > 0
